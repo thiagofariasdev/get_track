ef305033f8154df5047dadfc28368431c8bd5390
thiagofariasdev <thiago_fdutra@hotmail.com>
2019-04-11 13:10:55 +0000

    changes

---
+++ cases.php
@@ -0,0 +1,189 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Get Track</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="./css/font.css">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Work+Sans:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="./fonts/icomoon/style.css">
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/magnific-popup.css">
+  <link rel="stylesheet" href="./css/jquery-ui.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="./css/animate.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+  <link rel="stylesheet" href="./fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="./css/aos.css">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body style="background-image: url('image/img-1.jpg');">
+  <div class="site-wrap">
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+
+
+    <div class="site-navbar-wrap js-site-navbar bg-white">
+
+      <div class="container">
+        <div class="site-navbar bg-light">
+          <a href="/" class="font-weight-bold">
+            <img src="./image/logo.png" height="100" />
+          </a>
+          <div class="row align-items-center">
+            <div class="col-3">
+              <h4>
+                <a href="/">Get Track</a>
+              </h4>
+            </div>
+            <div class="col-9">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
+                      class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+                  <ul class="site-menu js-clone-nav d-none d-lg-block">
+                    <li><a href="./products">Produtos</a></li>
+                    <li class="active"><a href="./cases">Cases de sucesso</a></li>
+                    <li><a href="./contact">Contato</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-section block-14 nav-direction-white">
+      <div class="container">
+        <div class="row mb-5">
+          <div class="col-md-12">
+            <h2 class="site-section-heading text-center text-uppercase">Testimonies</h2>
+          </div>
+        </div>
+        <div class="nonloop-block-14 owl-carousel">
+          <div class="p-5">
+            <div class="d-block block-testimony w-75 mx-auto text-center">
+              <div class="person w-25 mx-auto mb-4">
+                <img src="images/person_1.jpg" alt="Image" class="img-fluid rounded-circle">
+              </div>
+              <div>
+                <h2 class="h5 mb-4">Katie Johnson</h2>
+                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias accusantium qui
+                  optio,
+                  possimus necessitatibus voluptate aliquam velit nostrum tempora ipsam!&rdquo;</blockquote>
+              </div>
+            </div>
+          </div>
+          <div class="p-5">
+            <div class="d-block block-testimony w-75 mx-auto text-center">
+              <div class="person w-25 mx-auto mb-4">
+                <img src="images/person_2.jpg" alt="Image" class="img-fluid rounded-circle">
+              </div>
+              <div>
+                <h2 class="h5 mb-4">Jun Mars</h2>
+                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias accusantium qui
+                  optio,
+                  possimus necessitatibus voluptate aliquam velit nostrum tempora ipsam!&rdquo;</blockquote>
+              </div>
+            </div>
+          </div>
+          <div class="p-5">
+            <div class="d-block block-testimony w-75 mx-auto text-center">
+              <div class="person w-25 mx-auto mb-4">
+                <img src="images/person_3.jpg" alt="Image" class="img-fluid rounded-circle">
+              </div>
+              <div>
+                <h2 class="h5 mb-4">Shane Holmes</h2>
+                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias accusantium qui
+                  optio,
+                  possimus necessitatibus voluptate aliquam velit nostrum tempora ipsam!&rdquo;</blockquote>
+              </div>
+            </div>
+          </div>
+          <div class="p-5">
+            <div class="d-block block-testimony w-75 mx-auto text-center">
+              <div class="person w-25 mx-auto mb-4">
+                <img src="images/person_4.jpg" alt="Image" class="img-fluid rounded-circle">
+              </div>
+              <div>
+                <h2 class="h5 mb-4">Mark Johnson</h2>
+                <blockquote>&ldquo;Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias accusantium qui
+                  optio,
+                  possimus necessitatibus voluptate aliquam velit nostrum tempora ipsam!&rdquo;</blockquote>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-6">
+                <h3 class="footer-heading mb-4 text-white">Quick Menu</h3>
+                <ul class="list-unstyled">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="./products">Produtos</a></li>
+                  <li><a href="./cases">Cases de sucesso</a></li>
+                  <li><a href="./contact">Contato</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="col-md-12">
+              <h3 class="footer-heading mb-4 text-white">Social Icons</h3>
+            </div>
+            <div class="col-md-12">
+              <p>
+                <a href="#" class="pb-2 pr-2 pl-0"><span class="icon-facebook"></span></a>
+                <a href="#" class="p-2"><span class="icon-youtube"></span></a>
+                <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              <script>document.write(new Date().getFullYear());</script> All Rights Reserved | This template is made
+              with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
+                target="_blank">Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
---
+++ contact.php
@@ -0,0 +1,176 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <title>Get Track</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="./css/font.css">
+  <link href="https://fonts.googleapis.com/css?family=Oswald:400,700|Work+Sans:300,400,700" rel="stylesheet">
+  <link rel="stylesheet" href="./fonts/icomoon/style.css">
+  <link rel="stylesheet" href="./css/bootstrap.min.css">
+  <link rel="stylesheet" href="./css/magnific-popup.css">
+  <link rel="stylesheet" href="./css/jquery-ui.css">
+  <link rel="stylesheet" href="./css/owl.carousel.min.css">
+  <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+  <link rel="stylesheet" href="./css/bootstrap-datepicker.css">
+  <link rel="stylesheet" href="./css/animate.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+  <link rel="stylesheet" href="./fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="./css/aos.css">
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+
+<body style="background-image: url('image/img-1.jpg');">
+  <div class="site-wrap">
+    <div class="site-mobile-menu">
+      <div class="site-mobile-menu-header">
+        <div class="site-mobile-menu-close mt-3">
+          <span class="icon-close2 js-menu-toggle"></span>
+        </div>
+      </div>
+      <div class="site-mobile-menu-body"></div>
+    </div> <!-- .site-mobile-menu -->
+    <div class="site-navbar-wrap js-site-navbar bg-white">
+      <div class="container">
+        <div class="site-navbar bg-light">
+          <a href="/" class="font-weight-bold">
+            <img src="./image/logo.png" height="100" />
+          </a>
+          <div class="row align-items-center">
+            <div class="col-3">
+              <h4>
+                <a href="/">Get Track</a>
+              </h4>
+            </div>
+            <div class="col-9">
+              <nav class="site-navigation text-right" role="navigation">
+                <div class="container">
+                  <div class="d-inline-block d-lg-none ml-md-0 mr-auto py-3"><a href="#"
+                      class="site-menu-toggle js-menu-toggle text-black"><span class="icon-menu h3"></span></a></div>
+
+                  <ul class="site-menu js-clone-nav d-none d-lg-block">
+                    <li><a href="./products">Produtos</a></li>
+                    <li><a href="./cases">Cases de sucesso</a></li>
+                    <li class="active"><a href="./contact">Contato</a></li>
+                  </ul>
+                </div>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="site-blocks-cover inner-page overlay" style="background-image: url(image/img-1.jpg);" data-aos="fade"
+      data-stellar-background-ratio="0.5">
+      <div class="row m-0 align-items-center justify-content-center">
+        <div class="col-md-6 text-center" data-aos="fade">
+          <h1 class="text-uppercase">Contact Us</h1>
+          <span class="caption d-block text-white">An Insurance Company</span>
+        </div>
+      </div>
+    </div>
+    <div class="py-5 bg-light">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 relative">
+            <div class="fg-blue-cont">
+              <img src="./image/icone-1.png" style="height: 50%" />
+              <div class="fg-blue-name">
+                <h3 style="font-family: 'Default Font';color:#fff; ">Rafael Sousa Xavier</h3>
+              </div>
+            </div>
+            <div class="fg-blue">
+            </div>
+            <div style="height:30vh">
+              <div class="fg-blue-resp">
+                <h4 style="font-family: 'Default Font';">Rafael Sousa Xavier</h4>
+              </div>
+              <div style="padding: 17px;">
+                <h5><span class="icon-mail_outline"></span> rafael@gettrack.com.br</h5>
+                <p><span class="icon-phone"></span> (51) 3336 3699 | (51) 99735 4222</p>
+              </div>
+            </div>
+          </div>
+          <div class="col-lg-12 relative">
+            <div class="fg-blue-cont">
+              <img src="./image/icone-1.png" style="height: 50%" />
+              <div class="fg-blue-name">
+                <h3 style="font-family: 'Default Font';color:#fff; ">Cristiane Barcellos</h3>
+              </div>
+            </div>
+            <div class="fg-blue">
+            </div>
+            <div style="height:30vh">
+              <div class="fg-blue-resp">
+                <h4 style="font-family: 'Default Font';">Cristiane Barcellos</h4>
+              </div>
+              <div style="padding: 17px;">
+                <h5><span class="icon-mail_outline"></span> cristiane@gettrack.com.br</h5>
+                <p><span class="icon-phone"></span> (51) 3336 3699 | (51) 99735 4222</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <footer class="site-footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5">
+            <div class="row">
+              <div class="col-md-6">
+                <h3 class="footer-heading mb-4 text-white">Quick Menu</h3>
+                <ul class="list-unstyled">
+                  <li><a href="/">Home</a></li>
+                  <li><a href="./products">Produtos</a></li>
+                  <li><a href="./cases">Cases de sucesso</a></li>
+                  <li><a href="./contact">Contato</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6">
+            <div class="col-md-12">
+              <h3 class="footer-heading mb-4 text-white">Social Icons</h3>
+            </div>
+            <div class="col-md-12">
+              <p>
+                <a href="#" class="pb-2 pr-2 pl-0"><span class="icon-facebook"></span></a>
+                <a href="#" class="p-2"><span class="icon-youtube"></span></a>
+                <a href="#" class="p-2"><span class="icon-instagram"></span></a>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row pt-5 mt-5 text-center">
+          <div class="col-md-12">
+            <p>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+              Copyright &copy;
+              <script>document.write(new Date().getFullYear());</script> All Rights Reserved | This template is made
+              with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
+                target="_blank">Colorlib</a>
+              <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  </div>
+  <script src="js/jquery-3.3.1.min.js"></script>
+  <script src="js/jquery-migrate-3.0.1.min.js"></script>
+  <script src="js/jquery-ui.js"></script>
+  <script src="js/popper.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+  <script src="js/owl.carousel.min.js"></script>
+  <script src="js/jquery.stellar.min.js"></script>
+  <script src="js/jquery.countdown.min.js"></script>
+  <script src="js/jquery.magnific-popup.min.js"></script>
+  <script src="js/bootstrap-datepicker.min.js"></script>
+  <script src="js/aos.js"></script>
+  <script src="js/main.js"></script>
+</body>
+
+</html>
