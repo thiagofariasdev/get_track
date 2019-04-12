cc7037fc68ed211f7db26c4728f18a64bd7c94ba
thiagofariasdev <thiago_fdutra@hotmail.com>
2019-04-12 11:18:45 -0300

    changes

--- contact.php
+++ contact.php
@@ -52,8 +52,8 @@
       data-stellar-background-ratio="0.5">
       <div class="row m-0 align-items-center justify-content-center">
         <div class="col-md-6 text-center" data-aos="fade">
-          <h1 class="text-uppercase">Contact Us</h1>
-          <span class="caption d-block text-white">An Insurance Company</span>
+          <h1 class="text-uppercase">ENTRAR EM CONTATO</h1>
+          <span class="caption d-block text-white">Uma empresa de segurança</span>
         </div>
       </div>
     </div>
