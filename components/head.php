33cff6c706f9a46289a9d99636009c1622118f2f
thiagofariasdev <thiago_fdutra@hotmail.com>
2019-04-12 16:16:09 -0300

    changes

---
+++ components/head.php
@@ -0,0 +1,53 @@
+<title>Get Track</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="shortcut icon" href="./image/favicon.png" type="image/png">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./css/font.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="https://fonts.googleapis.com/css?family=Oswald:400,700|Work+Sans:300,400,700">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./fonts/icomoon/style.css">
+<link rel="stylesheet" href="./css/bootstrap.min.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" disabled href="./css/magnific-popup.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./css/jquery-ui.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./css/owl.carousel.min.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./css/owl.theme.default.min.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" disabled href="./css/bootstrap-datepicker.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./css/animate.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./fonts/flaticon/font/flaticon.css">
+<link rel="preload" onload="this.rel = 'stylesheet'" as="style" href="./css/aos.css">
+<link rel="stylesheet" href="./css/style.css">
+<style>
+  .block{
+    width: 100%;
+  }
+  i.icon-check{
+    color: #28a745
+  }
+  i.icon-remove{
+    color: #f00
+  }
+  span.caption.d-block.text-white{
+    text-shadow: 1px 1px #000;
+  }
+  div.owl-next{
+    background-color: transparent !important;
+  }
+  div.owl-prev{
+    background-color: transparent !important;
+  }
+  @media only screen and (max-width: 900px ){
+    h1.font-secondary.font-weight-bold.text-uppercase{
+      font-size: 4rem;
+    }
+  }
+  @media only screen and (max-width: 700px ){
+    h1.font-secondary.font-weight-bold.text-uppercase{
+      font-size: 3rem;
+    }
+  }
+  @media only screen and (max-width: 500px ){
+    h1.font-secondary.font-weight-bold.text-uppercase{
+      font-size: 2rem;
+    }
+  }
+</style>
