f121109cf01d89fc056f0f176b829d644f61860d
thiagofariasdev <thiago_fdutra@hotmail.com>
2019-04-15 10:08:53 -0300

    changes

--- contact.php
+++ contact.php
@@ -9,7 +9,7 @@
   <div class="site-wrap">
     <?php include('./components/header.php');?>
 
-    <div class="site-blocks-cover inner-page overlay" style="background-image: url(image/img-1.jpg);" data-aos="fade"
+    <!-- div class="site-blocks-cover inner-page overlay" style="background-image: url(image/img-1.jpg);" data-aos="fade"
       data-stellar-background-ratio="0.5">
       <div class="row m-0 align-items-center justify-content-center">
         <div class="col-md-6 text-center" data-aos="fade">
@@ -17,7 +17,7 @@
           <span class="caption d-block text-white">Uma empresa de segurança</span>
         </div>
       </div>
-    </div>
+    </div -->
     <div class="py-5 bg-light">
       <div class="container">
         <div class="row">
