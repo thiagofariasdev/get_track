9eddcc28e9bc970ad9670bf1bc154152c6742d1a
thiagofariasdev <thiago_fdutra@hotmail.com>
2019-04-16 11:22:20 -0300

    changes

--- components/head.php
+++ components/head.php
@@ -14,12 +14,12 @@
 <link rel="preload" as="style" href="https://cdn.jsdelivr.net/npm/mediaelement@4.2.7/build/mediaelementplayer.min.css">
 <link rel="preload" as="style" href="/fonts/flaticon/font/flaticon.css">
 
+<link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/font.css">
 <link rel="stylesheet" href="/fonts/icomoon/style.css">
 <link rel="stylesheet" href="/css/magnific-popup.css">
 <link rel="stylesheet" href="/css/bootstrap-datepicker.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oswald:400,700|Work+Sans:300,400,700">
-<link rel="stylesheet" href="/css/bootstrap.min.css">
 <link rel="stylesheet" href="/css/jquery-ui.css">
 <link rel="stylesheet" href="/css/owl.carousel.min.css">
 <link rel="stylesheet" href="/css/animate.css">
