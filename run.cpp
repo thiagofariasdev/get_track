8870cb8b2e0de4fe5528a47e97b51ce6c10984ca
Thiago <thiago_fdutra@hotmail.com>
2019-04-24 09:41:19 -0300

    changes

--- run.cpp
+++ run.cpp
@@ -2,6 +2,15 @@
 
 using namespace std;
 
+/**
+ * 
+ *  Install make `sudo apt install make`
+ *  run make by `make run.cpp -B run`
+ *  run server by `./run`
+ * 
+ */
+
 int main(){
+    // change ip address here:
     system("php -S 192.168.0.12:3000");
 }
