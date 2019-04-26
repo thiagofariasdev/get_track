bcea23be2046944a97d3ec16dc0f8bb304490f67
Thiago <thiago_fdutra@hotmail.com>
2019-04-26 12:48:20 -0300

    changes

--- components/footer.php
+++ components/footer.php
@@ -48,12 +48,8 @@
     <div class="row pt-5 mt-5 text-center">
       <div class="col-md-12">
         <p>
-          <!-- 
           Copyright &copy;
-          <?php echo date('Y', time());?> All Rights Reserved | This template is made
-          with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com"
-            target="_blank">Colorlib</a>
-          Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
+          <?php echo date('Y', time());?> All Rights Reserved 
         </p>
       </div>
     </div>
